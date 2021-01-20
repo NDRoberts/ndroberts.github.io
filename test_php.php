9b842877250801232fcce1fec5d65317f5d24d8e
Nate Roberts <robernat@msudenver.edu>
2021-01-19 18:19:54 -0700

    Adding file to test combining HTML with PHP

---
+++ test_php.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Test of PHP Implementation</title>
+    </head>
+    <body>
+        <div>
+            <h1>This is a test of combining HTML and PHP code.</h1>
+        </div>
+        <main>
+            <p id="html">This paragraph is included in the HTML code.</p>
+            <?php echo "<p id='pheep'>This paragraph is generated with inline PHP.</p>"; ?>
+        </main>
+    </body>
+</html>
